4bb387c28ece4461aff2832434c873126e249b78
Tobiaha <122260640+Tobiaha@users.noreply.github.com>
2025-11-24 20:47:40 +0200

    couple assignments

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const leapyear = year % 4 === 0;
+  const leapiestyear = year % 4 || year % 400 === 0;
+  const century = year % 100 === 0;
 
+  if (leapyear && (!century || leapiestyear)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
